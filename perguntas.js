e9de99fc796aa8a66c8c48d73de96f6b255f4cf5
Mariaclaraoliveira2508 <souza.oliveira.maria2508@escola.pr.gov.br>
2025-08-14 14:39:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+'Química',
+'Qual o modelo atômico de Thomson?',
+'Modelo pudim de passas.'
+)
+
+```
+criaCartao(
+'Química',
+'Qual o símbolo químico do ouro?',
+'Au.'
+)
+criaCartao(
+'Química',
+'Qual a fórmula química do sal de cozinha?',
+'NaCl.'
+)
+criaCartao(
+'Química',
+'O que é uma mistura homogênea?',
+'Uma mistura com apenas uma fase visível.'
+)
+criaCartao(
+'Química',
+'Qual a passagem do estado sólido para o líquido?',
+'Fusão.'
+)
+criaCartao(
+'Química',
+'Quem propôs o modelo atômico planetário?',
+'Rutherford.'
+)
+criaCartao(
+'Química',
+'O que é um elemento químico?',
+'Uma substância formada por apenas um tipo de átomo.'
+)
+criaCartao(
+'Química',
+'Qual o gás mais abundante na atmosfera da Terra?',
+'Nitrogênio.'
+)
+criaCartao(
+'Química',
+'O que mede a escala de pH?',
+'A acidez ou basicidade de uma solução.'
+)
+```
